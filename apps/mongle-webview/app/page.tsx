632204333f48a2a32c6abb8e9e4c168604c75841
gbwlxhd97 <gbwlxhd97@naver.com>
2024-12-05 00:26:49 +0900

    [feat]: WIP..common shadcn-ui

--- apps/mongle-webview/app/page.tsx
+++ apps/mongle-webview/app/page.tsx
@@ -1,7 +1,8 @@
+import { Drawer } from '@repo/ui';
 import { cookies } from 'next/headers';
 
 export default function Home() {
   const cookieStore = cookies();
   const cookieValue = cookieStore.get('AUTH')?.value;
-  return <div>{cookieValue}</div>;
+  return <div>{cookieValue} <Drawer /></div>;
 }
