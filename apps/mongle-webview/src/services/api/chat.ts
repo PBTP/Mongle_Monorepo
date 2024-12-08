7c45cee701c9c4f821cb9df9f05b69513331be98
VictoryJu <kob0805@naver.com>
2024-12-08 23:57:26 +0900

    [fix]: Api Response 규격에 맞게 변경

--- apps/mongle-webview/src/services/api/chat.ts
+++ apps/mongle-webview/src/services/api/chat.ts
@@ -1,18 +1,20 @@
 import { ChatMessages, ChatRoom, ReqChatRoomMessages } from '../types/chat';
+import { ApiResponse } from '../types/instance';
 import createApi from './instance/fetch-instance';
 
 const createChatApi = (serverToken?: string) => {
   const api = createApi(serverToken);
 
   return {
-    fetchChatRooms: (): Promise<ChatRoom[]> => api<ChatRoom[]>('/v1/chat/room'),
+    fetchChatRooms: (): Promise<ApiResponse<ChatRoom[]>> =>
+      api<ApiResponse<ChatRoom[]>>('/v1/chat/room'),
     fetchChatRoomMessages: ({
       chatRoomId,
       cursor = '0',
       limit = '20',
       next = cursor,
-    }: ReqChatRoomMessages): Promise<ChatMessages> =>
-      api<ChatMessages>(`/v1/chat/room/${chatRoomId}/message`, {
+    }: ReqChatRoomMessages): Promise<ApiResponse<ChatMessages>> =>
+      api<ApiResponse<ChatMessages>>(`/v1/chat/room/${chatRoomId}/message`, {
         params: {
           chatRoomId,
           cursor,
