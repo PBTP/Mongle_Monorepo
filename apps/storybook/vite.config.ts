7ae30a3eb3779377494e4d05d87a55b0babd602c
gbwlxhd97 <gbwlxhd97@naver.com>
2025-03-01 23:23:01 +0900

    [set] setup apps/storybook

---
+++ apps/storybook/vite.config.ts
@@ -0,0 +1,15 @@
+//apps/storybook/vite.config.ts
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@mgmg/ui': resolve(__dirname, '../../packages/ui'),
+    }
+  }
+});
