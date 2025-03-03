480d5748581ecc4bc7244c5dfee796243746f76c
VictoryJu <kob0805@naver.com>
2025-03-04 02:22:27 +0900

    [feat] Native에서 사용할 calendar page 추가

---
+++ apps/mongle-webview/app/calendar/_components/calendar-drawer.tsx
@@ -0,0 +1,72 @@
+'use client';
+import CMCalendar from '@/components/ui/common/Calendar/Calendar';
+import {
+  Text,
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+  Button,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+  DrawerClose,
+} from '@mgmg/ui/components/ui';
+import { CloseIcon } from '@mgmg/ui/components/icons';
+import React, { useState } from 'react';
+import { CalendarSelectedDate } from '@/components/ui/common/Calendar/calendar-type';
+
+interface CalendarDrawerProps {
+  isOpen?: boolean;
+  asChild?: React.ReactNode;
+}
+
+const CalendarDrawer = ({ isOpen = true, asChild }: CalendarDrawerProps) => {
+  const [selectedDate, setSelectedDate] = useState<CalendarSelectedDate>(
+    new Date()
+  );
+  const disabled = !selectedDate;
+
+  const onConfirm = () => {
+    //TODO Native에 날짜 전달
+  };
+
+  const handleDateChange = (date: CalendarSelectedDate) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <Drawer open={isOpen}>
+      <DrawerTrigger asChild={!!asChild}>
+        {asChild ? asChild : <Text>날짜 선택</Text>}
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle className="text-center relative">
+            <Text typography="md-m">날짜 선택</Text>
+            <DrawerClose asChild>
+              <CloseIcon
+                className="absolute right-0 top-1/2 -translate-y-1/2"
+                width={24}
+                height={24}
+              />
+            </DrawerClose>
+          </DrawerTitle>
+        </DrawerHeader>
+        <DrawerDescription asChild>
+          <div className="px-4">
+            <div className="mb-6">
+              <CMCalendar onDateChange={handleDateChange} />
+            </div>
+            <div className="w-full pb-3">
+              <Button fullWidth disabled={disabled} onClick={onConfirm}>
+                확인
+              </Button>
+            </div>
+          </div>
+        </DrawerDescription>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default CalendarDrawer;
---
+++ apps/mongle-webview/app/calendar/page.tsx
@@ -0,0 +1,7 @@
+import CalendarDrawer from './_components/calendar-drawer';
+
+const CalendarPage = () => {
+  return <CalendarDrawer />;
+};
+
+export default CalendarPage;
