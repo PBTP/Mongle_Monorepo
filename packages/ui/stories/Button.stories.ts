e4c9059c9480f9d1ae966559fe0ef1a879774808
gbwlxhd97 <gbwlxhd97@naver.com>
2025-03-21 23:00:08 +0900

    [feat]: button story argTypes를 이용해서 button props 추가

--- packages/ui/stories/Button.stories.ts
+++ packages/ui/stories/Button.stories.ts
@@ -5,28 +5,60 @@ import { Button } from '@mgmg/ui/components/ui/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'UI/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
+    componentSubtitle: '몽글몽글의 버튼 컴포넌트입니다.',
+    docs: {
+      description: {
+        component: 'variant, size, disabled 속성을 통해 버튼의 종류, 크기, 비활성 상태를 설정할 수 있습니다.',
+      },
+    },
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  argTypes: {
+    
+    variant: {
+        type: 'select',
+        options: ['default', 'destructive', 'outline'],
+        description: '버튼의 종류입니다.',
+    },
+    size: {
+        type: 'select',
+        options: ['default', 'sm', 'lg', 'icon'],
+        description: '버튼의 크기입니다.',
+    },
+    disabled: {
+      type: 'boolean',
+      description: '버튼의 비활성 상태입니다.',
+    },
+  },
   args: { onClick: fn() },
+
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     variant: 'default',
     children: 'Button',
   },
 };
 
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+    children: 'Button',
+  },
+};
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Button',
+  },
+};
